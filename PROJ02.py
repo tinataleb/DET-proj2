6e59b5c03af01dfb06b4463ede24479b90c63caa
kwsong <kwsong@berkeley.edu>
2019-10-21 03:15:24 +0100

    demo code

--- PROJ02.py
+++ PROJ02.py
@@ -20,7 +20,7 @@ import time
 from adafruit_crickit import crickit
 from adafruit_seesaw.neopixel import NeoPixel
  
-num_pixels = 14  # Number of pixels driven from Crickit NeoPixel terminal
+num_pixels = 18  # Number of pixels driven from Crickit NeoPixel terminal
  
 # The following line sets up a NeoPixel strip on Seesaw pin 20 for Feather
 pixels = NeoPixel(crickit.seesaw, 20, num_pixels)
@@ -28,6 +28,7 @@ pixels = NeoPixel(crickit.seesaw, 20, num_pixels)
 # Audio recording parameters, set for our USB mic.
 RATE = 48000 # this should be 48000 for new mic; 44100 for old
 CHUNK = int(RATE / 10)  # 100ms
+greeted = False
 
 credential_path = "/home/pi/DET-2019-d3b82e6383ae.json" #replace with your file name!
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=credential_path
@@ -46,8 +47,8 @@ POT_SERVO = crickit.servo_3
 # original servo angles
 original_reaction_angle = 110
 original_cumulative_angle = 90
-original_pot_angle = 60
-cumulative_delta_angle = 15
+original_pot_angle = 25
+cumulative_delta_angle = 30
 cumulative_max_angle = 130
 
 # colors
@@ -170,7 +171,7 @@ def listen_print_loop(responses):
 
         else:
             print(transcript + overwrite_chars)
-            
+            #transcript = "oh no I'm running late"
             # Make a document from the transcript (needed for sentiment analysis?)
             type_ = enums_lang.Document.Type.PLAIN_TEXT
             language = "en"
@@ -208,8 +209,12 @@ def decide_action(transcript, sentiment):
     '''
     #here we're using some simple code on the final transcript from
     #GCP to figure out what to do, how to respond.
+    global greeted
+    
     if re.search('good morning',transcript, re.I):
-        greet()
+        if not greeted:
+            greet()
+            greeted = True
     elif sentiment > 0:
         act_happy(sentiment)
     elif sentiment < 0:
@@ -242,8 +247,8 @@ def act_happy(sentiment):
         
     # the bunch of flowers increases a little in height
     time.sleep(2)
-    if CUMULATIVE_SERVO.angle <= cumulative_max_angle - cumulative_delta_angle:
-        CUMULATIVE_SERVO.angle += cumulative_delta_angle
+    if CUMULATIVE_SERVO.angle <= int(cumulative_max_angle - cumulative_delta_angle*(1+sentiment)):
+        CUMULATIVE_SERVO.angle += int(cumulative_delta_angle * (1 + sentiment))
     else: # the flower is already as happy as can be
         act_overlyhappy()
 
@@ -253,6 +258,7 @@ def act_neutral():
         
 def act_overlyhappy():
     # placeholder function for doing something when the flower is already as happy as can be
+    shake_pot()
     sound_file = "dance.wav"
     pygame.mixer.music.load(sound_file)
     pygame.mixer.music.play()
@@ -289,7 +295,7 @@ def act_sad(score):
         CUMULATIVE_SERVO.angle -= cumulative_delta_angle
     else: # the flower is already as sad as can be
         act_overlysad()
-
+    time.sleep(2)
     REACTION_SERVO.angle = original_reaction_angle - 10
     
 def greet():
@@ -298,6 +304,7 @@ def greet():
     pygame.mixer.music.play()
     happy_lights()
     shake_pot()
+    shake_pot()
     REACTION_SERVO.angle = original_reaction_angle
     CUMULATIVE_SERVO.angle = original_cumulative_angle
     POT_SERVO.angle = original_pot_angle
@@ -305,7 +312,7 @@ def greet():
 def happy_lights():
     # celebratory LED lights
     polarity = 0
-    for j in range(20):
+    for j in range(40):
         for i in range(num_pixels):
             rc_index = (i * 256 // 10) + j*5
             pixels[i] = wheel(rc_index & 255)
@@ -329,7 +336,7 @@ def shake_pot():
     delay = 0.2
     shake_angle = 20
     POT_SERVO.angle += shake_angle
-    for i in range(5):
+    for i in range(4):
         time.sleep(delay)
         POT_SERVO.angle -= 2*shake_angle
         time.sleep(delay)
@@ -362,8 +369,8 @@ def main():
     REACTION_SERVO.angle = original_reaction_angle
     CUMULATIVE_SERVO.angle = original_cumulative_angle
     POT_SERVO.angle = original_pot_angle
-    
     pixels.fill(OFF)
+#    has_greeted = 0
     '''
     delta = 20
     for i in range(3):
@@ -378,7 +385,23 @@ def main():
     '''
 
     language_code = 'en-US'  # a BCP-47 language tag
-
+    #time.sleep(3)
+    #greet()
+    #time.sleep(5)
+    #act_happy(1)
+    #time.sleep(2)
+    #act_sad(-1)
+    #time.sleep(2)
+    #CUMULATIVE_SERVO.angle = 130
+    #act_overlyhappy()
+    #time.sleep(2)
+    #CUMULATIVE_SERVO.angle = 0
+    #act_sad(-1)
+    #time.sleep(2)
+    #act_happy(1)
+    #time.sleep(1)
+    #act_overlyhappy()
+    
     #set up a client
     #make sure GCP is aware of the encoding, rate 
     config = types_speech.RecognitionConfig(
